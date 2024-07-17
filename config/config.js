31a0e37998007879a3b4e12b61476cd166ce0e19
Hafizh Akmal Fauzi <akmal.fauzi19@gmail.com>
2024-07-17 23:54:16 +0700

    fix: config

---
+++ config/config.js
@@ -0,0 +1,20 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+export default {
+    development: {
+        username: process.env.DBUSER,
+        password: process.env.DBPASSWORD,
+        database: process.env.DBDATABASE,
+        host: process.env.DBHOST,
+        dialect: "mysql",
+    },
+    production: {
+        username: process.env.DBUSER,
+        password: process.env.DBPASSWORD,
+        database: process.env.DBDATABASE,
+        host: process.env.DBHOST,
+        dialect: "mysql",
+        ssl: 'require'
+    }
+}
